638904e45c55cda2534452cac04f0c59f700f5f2
lubinga <pfatsch@icloud.com>
2024-07-13 20:43:00 +0200

    Added some Design and Message Model

--- chat/views.py
+++ chat/views.py
@@ -10,21 +10,21 @@ def index(request: HttpRequest):
     # )
 
     # for debug_toolbar
-    print("From View: Request User:", request.user)
-    print("From View: Request Method:", request.method)
-    print("From View: Request Path:", request.path)
+    # print("From View: Request User:", request.user)
+    # print("From View: Request Method:", request.method)
+    # print("From View: Request Path:", request.path)
 
     # CAUTION for testing purpose only as this reveals sensitive data
-    for attr in dir(request):
-        if not attr.startswith("_"):
-            try:
-                print(f"{attr}: {getattr(request, attr)}")
-            except Exception as e:
-                print(f"An error occurred.\n{e}")
+    # for attr in dir(request):
+    #     if not attr.startswith("_"):
+    #         try:
+    #             print(f"{attr}: {getattr(request, attr)}")
+    #         except Exception as e:
+    #             print(f"An error occurred.\n{e}")
 
     # this refers to the templatesx within the app itself
     return render(
         request,
         "chat/index.html",
-        context={"content": "Some random content!"},
+        context={"content": "Some random content directly from the render context!"},
     )
